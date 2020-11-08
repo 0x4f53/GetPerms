408ef5adcc739575ee11bf49f332b15c0ad090fd
Owais Shaikh <0x4f@tuta.io>
2020-11-09 02:54:42 +0530

    Boilerplate commit

--- app/src/main/java/getperms_demo/MainActivity.java
+++ app/src/main/java/getperms_demo/MainActivity.java
@@ -1,23 +1,26 @@
+/*
+*
+* getPerms usage:
+*   getPerms("ALL"); - all permissions for all packages
+*   getPerms("GRANTED"); - granted permissions for all packages
+*   getPerms("package_name", "ALL"); - all permissions for a specific package
+*   getPerms("package_name", "GRANTED"); - granted permissions for a specific package
+*
+* */
+
 package getperms_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
-
 import com.example.getperms_demo.R;
 
 import java.util.HashMap;
@@ -25,124 +28,99 @@ import java.util.List;
 
 import static android.content.pm.PackageInfo.REQUESTED_PERMISSION_GRANTED;
 import static android.content.pm.PackageManager.GET_PERMISSIONS;
+import static android.content.pm.PermissionInfo.PROTECTION_DANGEROUS;
+import static android.content.pm.PermissionInfo.PROTECTION_SIGNATURE;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Get the application context
-        Context mContext = getApplicationContext();
-        Activity mActivity = MainActivity.this;
-        // Get the widgets reference from xml layout
-        Spinner mSpinner = findViewById(R.id.spinner);
-        mTextView = findViewById(R.id.text);
-        // Make text view scrollable
-        mTextView.setMovementMethod(new ScrollingMovementMethod());
-        // Get the installed package list
-        HashMap<String,String> map = getInstalledPackages();
-        // Get the values array from hash map
-        final String[] values = map.values().toArray(new String[0]);
-        final String[] keys = map.keySet().toArray(new String[0]);
-        // Initialize a new array adapter instance
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                mActivity,
-                android.R.layout.simple_spinner_item,
-                values
-        );
-        // Set the drop down view resource for array adapter
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Data bind the spinner with array adapter
-        mSpinner.setAdapter(adapter);
-        // Set an item selected listener for the spinner object
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String packageName = keys[i];
-                String label = values[i];
-                mTextView.setText(label + "\n" + packageName+"\n\n");
-                // Display the selected package granted permissions list
-                mTextView.append(getPermissionsByPackageName(packageName));
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
 
-            }
-        });
+        TextView output1 = (TextView)findViewById(R.id.method1output);
+        TextView output2 = (TextView)findViewById(R.id.method2output);
+        TextView output3 = (TextView)findViewById(R.id.method3output);
+        TextView output4 = (TextView)findViewById(R.id.method4output);
+
+        output1.setText(getPerms("ALL"));
+        output2.setText(getPerms("GRANTED"));
+        output3.setText(getPerms("com.google.android.networkstack.tethering", "ALL"));
+        output4.setText(getPerms("com.google.android.networkstack.tethering", "GRANTED"));
     }
 
-    // Custom method to get all installed package names
-    protected HashMap<String,String> getInstalledPackages(){
-        PackageManager packageManager = getPackageManager();
-        // Initialize a new intent
-        Intent intent = new Intent(Intent.ACTION_MAIN,null);
-        // Set the intent category
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        // Set the intent flags
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        // Initialize a new list of resolve info
-        List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(intent,0);
-        // Initialize a new hash map of package names and application label
-        HashMap<String,String> map = new HashMap<>();
-        // Loop through the resolve info list
-        for(ResolveInfo resolveInfo : resolveInfoList){
-            // Get the activity info from resolve info
-            ActivityInfo activityInfo = resolveInfo.activityInfo;
-            // Get the package name
-            String packageName = activityInfo.applicationInfo.packageName;
-            // Get the application label
-            String label = (String) packageManager.getApplicationLabel(activityInfo.applicationInfo);
-            // Put the package name and application label to hash map
-            map.put(packageName,label);
-        }
-        return map;
-    }
-    // Custom method to get app requested and granted permissions from package name
-    protected String getPermissionsByPackageName(String packageName){
-        // Initialize a new string builder instance
+    /*
+       1. getPerms("ALL"); - all permissions for all packages
+       AND
+       2. getPerms("GRANTED"); - granted permissions for all packages
+    */
+
+    protected String getPerms(String mode){
         StringBuilder builder = new StringBuilder();
         try {
-            // Get the package info
-            PackageInfo packageInfo = getPackageManager().getPackageInfo(packageName, GET_PERMISSIONS);
-            // Permissions counter
-            int counter = 1;
-            /*
-                PackageInfo
-                    Overall information about the contents of a package. This corresponds to all of
-                    the information collected from AndroidManifest.xml.
-            */
-            /*
-                String[] requestedPermissions
-                    Array of all <uses-permission> tags included under <manifest>, or null if there
-                    were none. This is only filled in if the flag GET_PERMISSIONS was set. This list
-                    includes all permissions requested, even those that were not granted or known
-                    by the system at install time.
-            */
-            /*
-                int[] requestedPermissionsFlags
-                    Array of flags of all <uses-permission> tags included under <manifest>, or null
-                    if there were none. This is only filled in if the flag GET_PERMISSIONS was set.
-                    Each value matches the corresponding entry in requestedPermissions, and will
-                    have the flag REQUESTED_PERMISSION_GRANTED set as appropriate.
-            */
-            /*
-                int REQUESTED_PERMISSION_GRANTED
-                    Flag for requestedPermissionsFlags: the requested permission is currently
-                    granted to the application.
-            */
-                    // Loop through the package info requested permissions
-            for (int i = 0; i < packageInfo.requestedPermissions.length; i++) {
-                if ((packageInfo.requestedPermissionsFlags[i] & REQUESTED_PERMISSION_GRANTED) != 0) {
-                    String requested_permissions =packageInfo.requestedPermissions[i];
-                    // To make permission name shorter: permission = permission.substring(permission.lastIndexOf(".")+1);
-                    builder.append("").append(counter).append(". ").append(requested_permissions).append("\n");
-                    counter++;
+            if (mode.equals("ALL")){
+                final PackageManager pm = getPackageManager();
+                List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                builder.append("{");
+                for (ApplicationInfo appInfo : packages) {
+                    String package_name = appInfo.packageName;
+                    builder.append("\n\"").append(package_name).append("\":[");
+                    PackageInfo packageInfo = getPackageManager().getPackageInfo(package_name, GET_PERMISSIONS);
+                    for (int i = 0; i < packageInfo.requestedPermissions.length; i++) {
+                        if ((packageInfo.requestedPermissionsFlags[i] & REQUESTED_PERMISSION_GRANTED) != 0) {
+                            String requested_permissions =packageInfo.requestedPermissions[i];
+                            // shorter permissions: permission = permission.substring(permission.lastIndexOf(".")+1);
+                            builder.append("\"").append(requested_permissions).append("\",");
+                        }
+                    }
+                    builder.deleteCharAt(builder.length()-1);
+                    builder.append("],");
                 }
+                builder.deleteCharAt(builder.length()-1);
+                builder.append("\n}");
+            } else if (mode.equals("GRANTED")){
+                final PackageManager pm = getPackageManager();
+                // TODO: 09/11/20 check if a permission has been granted to each app with if-else one by one and add to json.
+            } else {
+                throw new IllegalArgumentException("Illegal arguments passed to getPerms(). Please refer to the library guide at the top of the class file or at https://gitlab.com/ThomasCat/getperms.");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IllegalArgumentException wrongArgs){
+            return "Illegal arguments. Check documentation.";
+        } catch (PackageManager.NameNotFoundException nnfe) {
+            return "Couldn't find package.";
+        }
+        return builder.toString();
+    }
+
+    /*
+       3. getPerms("package_name", "ALL"); - all permissions for a specific package
+       AND
+       4. getPerms("package_name", "GRANTED"); - granted permissions for a specific package
+    */
+    protected String getPerms(String package_name, String mode){
+        StringBuilder builder = new StringBuilder();
+        try {
+            if (mode.equals("ALL")){
+                PackageInfo packageInfo = getPackageManager().getPackageInfo(package_name, GET_PERMISSIONS);
+                builder.append("{").append("\n\"").append(package_name).append("\":[");
+                for (int i = 0; i < packageInfo.requestedPermissions.length; i++) {
+                    if ((packageInfo.requestedPermissionsFlags[i] & REQUESTED_PERMISSION_GRANTED) != 0) {
+                        String requested_permissions =packageInfo.requestedPermissions[i];
+                        // shorter permissions: permission = permission.substring(permission.lastIndexOf(".")+1);
+                        builder.append("\"").append(requested_permissions).append("\",");
+                    }
+                }
+                builder.deleteCharAt(builder.length()-1);
+                builder.append("]").append("\n}");
+            } else if (mode.equals("GRANTED")){
+                final PackageManager pm = getPackageManager();
+                // TODO: 09/11/20 check if a permission has been granted with if-else one by one and add to json.
+            } else {
+                throw new IllegalArgumentException("Illegal arguments passed to getPerms(). Please refer to the library guide at the top of the class file or at https://gitlab.com/ThomasCat/getperms.");
+            }
+        } catch (IllegalArgumentException iae){
+            return "Illegal arguments. Check documentation.";
+        } catch (PackageManager.NameNotFoundException nnfe) {
+            return "Couldn't find package.";
         }
         return builder.toString();
     }
