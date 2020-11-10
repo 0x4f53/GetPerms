e5beedce2e9a863632d8e05191746a159443fb50
Owais Shaikh <0x4f@tuta.io>
2020-11-11 03:21:44 +0530

    -

--- GetPermsDemo/app/src/main/java/com/example/getperms_demo/MainActivity.java
+++ GetPermsDemo/app/src/main/java/com/example/getperms_demo/MainActivity.java
@@ -16,15 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         EditText editText = (EditText)findViewById(R.id.packagenameinput);
         TextView requested_specific_output = (TextView)findViewById(R.id.method3output);
+        TextView granted_specific_output = (TextView)findViewById(R.id.method4output);
         editText.setOnKeyListener((View.OnKeyListener) (v, keyCode, event) -> {
             if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
                 String package_name = editText.getText().toString();
                 GetPerms gp = new GetPerms(getApplicationContext());
                 requested_specific_output.setText(gp.getRequested(package_name).toString());
+                granted_specific_output.setText(gp.getGranted(package_name).toString());
             }
             return false;
         });
-        TextView requested_all_output = (TextView)findViewById(R.id.method1output);
+        TextView requested_all_output = findViewById(R.id.method1output);
         GetPerms gp = new GetPerms(getApplicationContext());
         requested_all_output.setText(gp.getRequested().toString());
     }
